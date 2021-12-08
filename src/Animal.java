aa5ad70a1d47a78b03c9e10f333adf643997b936
ThomasKesik <thomaskesik@gmail.com>
2021-12-08 21:28:12 +0100

    Zadanie 1 - Pets feeding process

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,34 @@
 public class Animal {
+    public final String species;
+    private Double weight;
+
+    public Animal(String species) {
+        this.species = species;
+        if(this.species == "Pies"){
+            this.weight = 20.5;
+        } else if (this.species == "Kot"){
+            this.weight = 3.4;
+        } else {
+            this.weight = 15.0;
+        }
+    }
+
+    public void feed(){
+        if(weight == 0){
+            System.out.println("Nie można karmić zwierzęcia które nie żyje");
+        } else {
+            this.weight++;
+        }
+    }
+
+    public void takeForAWalk(){
+        if(weight == 0){
+            System.out.println("Nie wyprowadzić zwierzęcia które nie żyje");
+        } else {
+            this.weight--;
+            if(this.weight < 0){
+                this.weight = 0.0;
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Animal animal = new Animal("Kot");
+        animal.takeForAWalk();
+        animal.feed();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.feed();
+        animal.takeForAWalk();
     }
 }
