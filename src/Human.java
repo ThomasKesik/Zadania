c25fc66a9e032916a247ec259c3b2862a2c90c53
ThomasKesik <thomaskesik@gmail.com>
2021-12-10 21:15:48 +0100

    Zadanie 3 i 4: get your salary & give yourself a rise

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,39 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Human {
     public Animal pet;
     public Car car;
+    private Double salary = 0.0;
+    private String lastSalaryText = "";
+
+    public Double getSalary(){
+        if(lastSalaryText.equals("")){
+            System.out.println("Wartość wypłaty wyświetlana jest pierwszy raz " + salary);
+        } else {
+            System.out.println(lastSalaryText);
+        }
+        lastSalaryText = "Ostatni raz wypłata wyświetlana była " + getActualDate() + " jej wartość wynosiła " + salary;
+        return salary;
+    }
+
+    private String getActualDate(){
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        return dateFormat.format(date);
+    }
+
+    public void setSalary(Double salary){
+        if(salary < 0){
+            System.out.println("Wartość wypłaty musi być większa równa 0 !!!");
+        } else {
+            System.out.println("Dane zostały wysłane do systemu księgowości !!!");
+            System.out.println("Należy odebrać aneks do umowy u pani Hani z kadr !!!");
+            System.out.println("ZUS i US wiedzą o wysokości twojej wypłaty nie ukrywaj jej !!! ");
+            this.salary = salary;
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
         Human human = new Human();
         Car car = new Car("Skoda", "Octavia", "1J8FFC7Y77D356964", 2000);
         human.car = car;
+
+        human.setSalary(-100.0);
+        human.getSalary();
+        human.getSalary();
+        human.setSalary(251.0);
+        Double salary = human.getSalary();
+        System.out.println(salary);
+
     }
 }
